72996234128250577f82d738c4667cb217e3268e
Your Name <you@example.com>
2023-05-20 23:43:04 -0400

    new textures file

--- NonEuclidean/main.cpp
+++ NonEuclidean/main.cpp
@@ -7,14 +7,75 @@
 #include "node.hpp"
 #include "SFML/Graphics.hpp"
 
+Player* loadMap() {
+    std::vector<std::vector<Node*>> world(16);
+    for (int i = 0; i <= 15; ++i) {
+        world[i].resize(30, nullptr);
+    }
+
+    for (int h = 0; h <= 15; h++) {
+        for (int w = 0; w <= 29; w++) {
+            switch (level[h][w]) {
+            case '.':
+            case 'x':
+                world[h][w] = new Floor;
+                break;
+            case 'R':
+                world[h][w] = new RedWall;
+                break;
+            case 'B':
+                world[h][w] = new BlueWall;
+                break;
+            case 'G':
+                world[h][w] = new GreenWall;
+                break;
+            case 'Y':
+                world[h][w] = new YellowWall;
+                break;
+            }
+        }
+    }
+
+    for (int h = 0; h <= 15; h++) {
+        for (int w = 0; w <= 29; w++) {
+            if (level[h][w] == '.') {
+                if (h > 0 && level[h - 1][w] != 'x') world[h][w]->addNeighbour(Direction::FORWARD, world[h - 1][w]);
+                if (h < 15 && level[h + 1][w] != 'x') world[h][w]->addNeighbour(Direction::BACK, world[h + 1][w]);
+                if (w > 0 && level[h][w - 1] != 'x') world[h][w]->addNeighbour(Direction::LEFT, world[h][w - 1]);
+                if (w < 29 && level[h][w + 1] != 'x') world[h][w]->addNeighbour(Direction::RIGHT, world[h][w + 1]);
+            }
+        }
+    }
+
+    world[5][7]->removeNeighbour(Direction::FORWARD);
+    world[5][7]->addNeighbour(Direction::FORWARD, world[11][22]);
+
+    world[11][22]->removeNeighbour(Direction::BACK);
+    world[11][22]->addNeighbour(Direction::BACK, world[5][7]);
+
+    world[12][4]->removeNeighbour(Direction::LEFT);
+    world[12][4]->addNeighbour(Direction::LEFT, world[12][8]);
+
+    world[12][8]->removeNeighbour(Direction::RIGHT);
+    world[12][8]->addNeighbour(Direction::RIGHT, world[12][4]);
+
+    Position pos;
+    pos.node = world[8][4];
+    pos.offset = 0.5 + 0.5i;
+    pos.angle = 0.9;
+    Player* player = new Player(pos);
+
+    return player;
+}
+
 int main() {
     sf::Image image;
     image.create(x_dim, y_dim);
     Player* player = loadMap();
 
-    sf::RenderWindow window(sf::VideoMode(x_dim*scale, y_dim*scale), "Non-Euclidean Game Engine");
+    sf::RenderWindow window(sf::VideoMode(x_dim * scale, y_dim * scale), "Non-Euclidean Game Engine");
     sf::Event e;
-    while (window.isOpen()) 
+    while (window.isOpen())
     {
         while (window.pollEvent(e))
         {
@@ -29,24 +90,3 @@ int main() {
 
     return 0;
 }
-
-Player* loadMap() {
-    std::vector<std::vector<Node*>> map(16);
-    for (int i = 0; i <= 15; ++i) {
-        map[i].resize(30, nullptr);
-    }
-
-    Position pos;
-    pos.node = map[8][4];
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
