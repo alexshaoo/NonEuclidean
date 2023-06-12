9184f0854ce0182082c7da71a970fe6fe493a63f
alexshaoo <alexshao90@gmail.com>
2023-06-12 00:13:50 -0400

    player movement mechanic

--- NonEuclidean/player.cpp
+++ NonEuclidean/player.cpp
@@ -23,5 +23,51 @@ void Player::rotate(double offset) {
 }
 
 void Player::move(Direction direction, double distance) {
-    ;
+    double offset;
+    switch (direction) {
+        case Direction::UP:
+            offset = 0.0;
+            break;
+        case Direction::LEFT:
+            offset = M_PI / 2.0;
+			break;
+        case Direction::DOWN:
+			offset = M_PI;
+            break;
+		case Direction::RIGHT:
+            offset = -M_PI / 2.0;
+            break;
+    }
+    std::complex<double> newPosition = this->position.offset + std::polar(distance, this->position.angle + offset);
+
+    // TODO: wall hitbox mechanic
+    Node* neighbour = nullptr;
+    if (newPosition.real() < 0.0) {
+        neighbour = this->position.node->getNeighbour(Direction::LEFT);
+        newPosition.real(fmod(newPosition.real()+1.0, 1.0));
+        newPosition.imag(this->position.offset.imag());
+    }
+    else if (newPosition.real() > 1.0) {
+        neighbour = this->position.node->getNeighbour(Direction::RIGHT);
+        newPosition.real(fmod(newPosition.real()+1.0, 1.0));
+        newPosition.imag(this->position.offset.imag());
+    }
+    else if (newPosition.imag() < 0.0) {
+		neighbour = this->position.node->getNeighbour(Direction::DOWN);
+		newPosition.real(this->position.offset.real());
+		newPosition.imag(fmod(newPosition.imag()+1.0, 1.0));
+    }
+    else if (newPosition.imag() > 1.0) {
+		neighbour = this->position.node->getNeighbour(Direction::UP);
+		newPosition.real(this->position.offset.real());
+		newPosition.imag(fmod(newPosition.imag()+1.0, 1.0));
+    }
+    else {
+        this->position.offset = newPosition;
+    }
+
+    if (neighbour != nullptr && !neighbour->isWall()) {
+		this->position.node = neighbour;
+		this->position.offset = newPosition;
+	}
 }
