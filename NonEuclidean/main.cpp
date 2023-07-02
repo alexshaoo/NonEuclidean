722c5de02f57852cf84e659e6ad0838d15fe04b3
alexshaoo <alexshao90@gmail.com>
2023-07-02 19:46:03 -0400

    refactor consts and changed exit system

--- NonEuclidean/main.cpp
+++ NonEuclidean/main.cpp
@@ -5,13 +5,13 @@
 
 
 std::vector<std::vector<Node*>> mapOne() {
-    std::vector<std::vector<Node*>> world(16);
-    for (int i = 0; i <= 15; ++i) {
-        world[i].resize(30, nullptr);
+    std::vector<std::vector<Node*>> world(MAP_HEIGHT);
+    for (int i = 0; i < MAP_HEIGHT; ++i) {
+        world[i].resize(MAP_WIDTH, nullptr);
     }
 
-    for (int h = 0; h <= 15; h++) {
-        for (int w = 0; w <= 29; w++) {
+    for (int h = 0; h < MAP_HEIGHT; h++) {
+        for (int w = 0; w < MAP_WIDTH; w++) {
             switch (level[h][w]) {
             case '.':
             case 'x':
@@ -33,13 +33,13 @@ std::vector<std::vector<Node*>> mapOne() {
         }
     }
 
-    for (int h = 0; h <= 15; h++) {
-        for (int w = 0; w <= 29; w++) {
+    for (int h = 0; h < MAP_HEIGHT; h++) {
+        for (int w = 0; w < MAP_WIDTH; w++) {
             if (level[h][w] == '.') {
                 if (h > 0 && level[h - 1][w] != 'x') world[h][w]->addNeighbour(Direction::UP, world[h - 1][w]);
-                if (h < 15 && level[h + 1][w] != 'x') world[h][w]->addNeighbour(Direction::DOWN, world[h + 1][w]);
+                if (h < MAP_HEIGHT-1 && level[h + 1][w] != 'x') world[h][w]->addNeighbour(Direction::DOWN, world[h + 1][w]);
                 if (w > 0 && level[h][w - 1] != 'x') world[h][w]->addNeighbour(Direction::LEFT, world[h][w - 1]);
-                if (w < 29 && level[h][w + 1] != 'x') world[h][w]->addNeighbour(Direction::RIGHT, world[h][w + 1]);
+                if (w < MAP_WIDTH-1 && level[h][w + 1] != 'x') world[h][w]->addNeighbour(Direction::RIGHT, world[h][w + 1]);
             }
         }
     }
@@ -59,9 +59,10 @@ std::vector<std::vector<Node*>> mapOne() {
     return world;
 }
 
-Player* loadMap() {
+Player* loadPlayer() {
     std::vector<std::vector<Node*>> world = mapOne();
 
+    // spawn at (8,4) facing 0.9 radians
     Position pos;
     pos.node = world[8][4];
     pos.offset = std::complex<double>(0.5, 0.5);
@@ -75,76 +76,136 @@ int main() {
     sf::Image image;
     sf::Texture texture;
     sf::Sprite sprite;
-    image.create(x_dim, y_dim);
-    texture.create(x_dim, y_dim);
+    image.create(X_DIM, Y_DIM);
+    texture.create(X_DIM, Y_DIM);
     texture.update(image);
     sprite.setTexture(texture);
     sprite.setPosition(0, 0);
     sprite.setScale(scale, scale);
-    Player* player = loadMap();
+    Player* player = loadPlayer();
 
-    sf::RenderWindow window(sf::VideoMode(x_dim * scale, y_dim * scale), "Non-Euclidean Game Engine");
+    // test 
+    bool inGame = true; // Start in the game
+    sf::RectangleShape button(sf::Vector2f(200, 50)); // create a button of size 200x50
+    button.setFillColor(sf::Color::Red); // set button color to red
+    button.setPosition((X_DIM * scale) / 2 - 100, (Y_DIM * scale) / 2 - 25); // center the button
+
+    sf::Text buttonText;
+    buttonText.setString("Quit");
+    buttonText.setCharacterSize(24);
+    buttonText.setFillColor(sf::Color::White);
+    buttonText.setStyle(sf::Text::Bold);
+    buttonText.setPosition((X_DIM * scale) / 2 - 30, (Y_DIM * scale) / 2 - 15); // roughly center text on the button
+    // test
+
+    sf::RenderWindow window(sf::VideoMode(X_DIM * scale, Y_DIM * scale), "Non-Euclidean Game Engine");
     window.setVerticalSyncEnabled(true);
+
     while (window.isOpen()) {
         sf::Event e;
         while (window.pollEvent(e)) {
             switch (e.type) {
-                /*case sf::Event::KeyPressed:
-                    if (e.key.code == sf::Keyboard::Left) {
-						player->turnLeft();
-					}
-                    if (e.key.code == sf::Keyboard::Right) {
-						player->turnRight();
-					}
-                    if (e.key.code == sf::Keyboard::Up) {
-						player->moveForward();
-					}
-                    if (e.key.code == sf::Keyboard::Down) {
-						player->moveBackward();
-					}
-					break;*/
             case sf::Event::KeyPressed:
                 switch (e.key.code) {
                 case sf::Keyboard::Escape:
-                    window.close();
+                    inGame = !inGame; // Switch between game and menu
                     break;
                 }
                 break;
             case sf::Event::Closed:
-				window.close();
-				break;
-			}
+                window.close();
+                break;
+            case sf::Event::MouseButtonPressed:
+                if (!inGame && button.getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+                    // Quit button pressed, close window
+                    window.close();
+                }
+                break;
+            }
         }
-
-        // scuffed rotation system
-        double rotate = 0.0;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            rotate += 0.04;
-        }
-	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            rotate -= 0.04;
-        }
-
-        player->rotate(rotate);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            player->move(Direction::UP, 0.05);
-	    }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		    player->move(Direction::DOWN, 0.05);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            player->move(Direction::LEFT, 0.05);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		    player->move(Direction::RIGHT, 0.05);
-	    }
 
         window.clear();
-        render(image, player);
-        texture.update(image);
-        window.draw(sprite);
+        if (inGame) {
+            // scuffed rotation system
+            double rotate = 0.0;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                rotate += PLAYER_ROTATION;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                rotate -= PLAYER_ROTATION;
+            }
+
+            player->rotate(rotate);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                player->move(Direction::UP, PLAYER_MOVE_DISTANCE);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                player->move(Direction::DOWN, PLAYER_MOVE_DISTANCE);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                player->move(Direction::LEFT, PLAYER_MOVE_DISTANCE);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                player->move(Direction::RIGHT, PLAYER_MOVE_DISTANCE);
+            }
+            render(image, player);
+            texture.update(image);
+            window.draw(sprite);
+        }
+        else {
+            window.clear(sf::Color::White);
+            window.draw(button);
+            window.draw(buttonText);
+        }
         window.display();
     }
+
+    //while (window.isOpen()) {
+    //    sf::Event e;
+    //    while (window.pollEvent(e)) {
+    //        switch (e.type) {
+    //            case sf::Event::KeyPressed:
+    //                switch (e.key.code) {
+    //                    case sf::Keyboard::Escape:
+    //                        window.close();
+    //                        break;
+    //                    }
+    //                    break;
+    //            case sf::Event::Closed:
+				//    window.close();
+				//    break;
+			 //   }
+    //    }
+
+    //    // scuffed rotation system
+    //    double rotate = 0.0;
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+    //        rotate += PLAYER_ROTATION;
+    //    }
+	   // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+    //        rotate -= PLAYER_ROTATION;
+    //    }
+
+    //    player->rotate(rotate);
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+    //        player->move(Direction::UP, PLAYER_MOVE_DISTANCE);
+	   // }
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+		  //  player->move(Direction::DOWN, PLAYER_MOVE_DISTANCE);
+    //    }
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+    //        player->move(Direction::LEFT, PLAYER_MOVE_DISTANCE);
+    //    }
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		  //  player->move(Direction::RIGHT, PLAYER_MOVE_DISTANCE);
+	   // }
+
+    //    window.clear();
+    //    render(image, player);
+    //    texture.update(image);
+    //    window.draw(sprite);
+    //    window.display();
+    //}
 
     return 0;
 }
